53df27d66e1fa6a0396c14bf3a13177d07e88cfb
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-08-28 10:08:10 +0530

    Initial Commit

---
+++ StopWatchSample/StopWatchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchSampleApp.swift
+//  StopWatchSample
+//
+//  Created by Ganpat Jangir on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
